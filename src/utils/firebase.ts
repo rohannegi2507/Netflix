781b7deb28a981ef0d2a235675ddcb2524525438
rohan <rohan.negi.dev@gmail.com>
2024-06-08 19:21:30 +0530

    add vite config for deployement

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { FirebaseApp, initializeApp } from "firebase/app";
-import { Analytics, getAnalytics } from "firebase/analytics";
 import { Auth, getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
