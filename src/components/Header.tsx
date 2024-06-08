f04dd1e96ac036a28347426ee66e289b15ef4ce0
rohan <rohan.negi.dev@gmail.com>
2024-06-08 19:07:57 +0530

    Fix login issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,40 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { auth } from '../utils/firebase'
 import { useNavigate } from 'react-router-dom'
-import { signOut } from 'firebase/auth'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { addUser, removeUser } from '../utils/userSlice'
+import { NETFLIX_LOGO } from '../utils/constant'
 
 const Header = () => {
-  const naviagte = useNavigate()
+  const navigate = useNavigate()
   const userInfo = useSelector((state) => state.user.userInfo)
+  const dispatch = useDispatch()
 
   useEffect(() => {
   }, [userInfo])
 
+  useEffect(() => {
+     const unsubscribeCallback = onAuthStateChanged(auth, (user)=>{
+       if(user){
+         const {uid, email,displayName, photoURL} = user;
+
+         dispatch(addUser({uid,email,displayName,photoURL}))
+         navigate('/browser')
+       } else {
+          dispatch(removeUser())
+          navigate('/')
+       }
+     })
+
+     return () => {
+      unsubscribeCallback()
+     }
+  },[])
+
   const handleSignOut = () => {
     try {
-      signOut(auth).then(() => {
-        naviagte("/login")
-      })
+      signOut(auth)
     } catch (e) {
       console.error("Signed out", e)
     }
@@ -25,7 +44,7 @@ const Header = () => {
     <header className='absolute z-1 w-full top-0 p-2 bg-gradient-to-b from-black flex justify-between'>
       <img
         className='w-44'
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={NETFLIX_LOGO}
         alt="netflix-logo"
       ></img>
       {userInfo ?
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,17 @@
 import React, { useRef, useState } from 'react'
 import Header from '../components/Header'
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, UserCredential } from 'firebase/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../utils/firebase';
 import { validateData } from '../utils/validate';
-import { useDispatch } from 'react-redux';
-import { addUser } from '../utils/userSlice';
-import { useNavigate } from 'react-router-dom';
+import { USER_AVTAR } from '../utils/constant';
 
 const Login = () => {
-  const [isNewUser, setIsNewUser] = useState(true)
+  const [isNewUserPage, setIsNewUser] = useState(true)
   const [errorString, setErrorString] = useState('')
 
   const fullName = useRef<HTMLInputElement>(null)
   const email = useRef<HTMLInputElement>(null)
   const password = useRef<HTMLInputElement>(null)
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -26,17 +21,12 @@ const Login = () => {
     try {
 
       if (validateData(email.current?.value, password.current?.value)) {
-        let userData
-        if (isNewUser) {
+        if (isNewUserPage) {
           const firebaseUser = await createUserWithEmailAndPassword(auth, email.current?.value, password.current?.value)
-          userData =  await updateProfile(firebaseUser.user, {displayName:fullName.current?.value, photoURL:"https://avatars.githubusercontent.com/u/49731384?v=4" })
-          dispatch(addUser({name: auth.currentUser?.displayName, email: auth.currentUser?.email, photoURL: auth.currentUser?.photoURL}))
-         
+          await updateProfile(firebaseUser.user, {displayName:fullName.current?.value, photoURL:USER_AVTAR })
         } else {
-          userData = await signInWithEmailAndPassword(auth, email.current?.value, password.current?.value)
-          dispatch(addUser({name: userData.user.displayName, email: userData.user.email, photoURL:userData.user.photoURL}))
+          await signInWithEmailAndPassword(auth, email.current?.value, password.current?.value)
         }
-        navigate('/browser')
       }
     } catch (e) {
       if(typeof e === 'string') {
@@ -52,21 +42,21 @@ const Login = () => {
 
       <div className="absolute mx-auto rounded-md text-center text-white bg-black w-4/12 flex flex-col gap-2 p-12 bg-opacity-80" >
         <form onSubmit={handleSubmit} className="text-white flex flex-col gap-3"  >
-          {<h1 className='text-2xl flex mb-2 p-2 font-bold'>{isNewUser ? ' Create' : ' Sign in'}</h1>}
-          {isNewUser ? <input ref={fullName} type='text' className="bg-black px-4 py-5 w-full bg-opacity-50" placeholder='Full Name' ></input> : null}
+          {<h1 className='text-2xl flex mb-2 p-2 font-bold'>{isNewUserPage ? ' Create' : ' Sign in'}</h1>}
+          {isNewUserPage ? <input ref={fullName} type='text' className="bg-black px-4 py-5 w-full bg-opacity-50" placeholder='Full Name' ></input> : null}
           <input ref={email} type='text' className="bg-black px-4 py-5 w-full" placeholder='Email or mobile number' ></input>
           <input ref={password} type='text' className="bg-black  px-4 py-5 w-full bg-" placeholder='Password'></input>
 
           <button
             type="submit" className='bg-red-600 text-white py-4 font-bold mt-4 border-1 rounded hover:bg-red-700' >
-            {isNewUser ? ' Create' : ' Sign in'}
+            {isNewUserPage ? ' Create' : ' Sign in'}
           </button>
           {errorString.length !== 0 ? <div>{errorString}</div> : <></>}
         </form>
 
         <p className='inline flex p-2'>
-          <span>{isNewUser ? 'New to Netflix ? ' : 'Aleady a user ? '}</span>
-          <button className="inline font-bold ml-1" onClick={() => { setIsNewUser(!isNewUser) }}>{isNewUser ? ' Sign up' : ' Sign in'}</button>
+          <span>{!isNewUserPage ? 'New to Netflix ? ' : 'Aleady a user ? '}</span>
+          <button className="inline font-bold ml-1" onClick={() => { setIsNewUser(!isNewUserPage) }}>{isNewUserPage ? ' Sign in' : ' Sign up'}</button>
         </p>
       </div>
 
