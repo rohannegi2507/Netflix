4872f07d8d43a20328f8b2aa11a99c9c99c653fa
rohan <rohan.negi.dev@gmail.com>
2024-06-09 01:02:30 +0530

    update vit config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix/' // replace <REPO_NAME> with your repository name
+  base: '/netflix/' // replace <REPO_NAME> with your repository name
 })
 
